266ce8cd12804d976a848984d537b6cefb8088cf
Haintrain <jelly.han01@gmail.com>
2018-02-03 21:23:25 +1300

    Fixing Stuff and Override Command Toggle

--- src/main/java/io/github/haintrain/soulbind/Soulbind.java
+++ src/main/java/io/github/haintrain/soulbind/Soulbind.java
@@ -1,5 +1,6 @@
 package io.github.haintrain.soulbind;
 
+import io.github.archemedes.customitem.CustomTag;
 import net.minegrid.module.crowns.CrownAPI;
 import net.minegrid.module.crowns.CrownEmporium;
 import net.minegrid.module.crowns.CrownIcon;
@@ -11,19 +12,8 @@ import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityShootBowEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.inventory.*;
-import org.bukkit.event.player.PlayerDropItemEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
-import org.bukkit.inventory.Inventory;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.*;
 
@@ -32,21 +22,38 @@ import static io.github.haintrain.soulbind.SoulbindEnchant.isSoulbound;
 @Info(name = "Soulbind", version = "1.0", color = ChatColor.WHITE, displayName = "Soulbind", dependencies = {CrownEmporium.class })
 public class Soulbind extends JavaModule implements ObeliskListener{
 
-    public SoulbindEnchant ench = new SoulbindEnchant(101);
+    private SoulbindEnchant ench = new SoulbindEnchant(101);
     private static transient Soulbind instance;
-    private transient CrownPurchase token;
+    private CrownPurchase token1, token5, token10;
 
     @Override
     public void onEnable() {
         SoulbindEnchant.getBound();
         Obelisk.registerCommands(this, this);
         instance = this;
-        token = CrownAPI.buildPurchase("Test Token", CrownIcon.build("More Test", Material.LEATHER_BOOTS)
+
+        registerEvents(new SoulbindListeners(ench, this));
+
+        token1 = CrownAPI.buildPurchase("Token x1", CrownIcon.build("Token x1", Material.LEATHER_BOOTS)
                 .setDesc("Test")
                 .build(), 500, player -> {
-            setToken(player);
+            setToken(player, 1);
         }, null).build();
-        CrownAPI.addPurchase(token);
+        CrownAPI.addPurchase(token1);
+
+        token5 = CrownAPI.buildPurchase("Token x5", CrownIcon.build("Token x5", Material.LEATHER_BOOTS)
+                .setDesc("Test")
+                .build(), 500, player -> {
+            setToken(player, 5);
+        }, null).build();
+        CrownAPI.addPurchase(token5);
+
+        token10 = CrownAPI.buildPurchase("Token x10", CrownIcon.build("Token x10", Material.LEATHER_BOOTS)
+                .setDesc("Test")
+                .build(), 500, player -> {
+            setToken(player, 10);
+        }, null).build();
+        CrownAPI.addPurchase(token10);
     }
 
     public void onDisable() {
@@ -56,7 +63,16 @@ public class Soulbind extends JavaModule implements ObeliskListener{
         return instance;
     }
 
-    @OCmd(cmd = "soulbind *", info = "Makes item soulbound to you")
+    public void onPlayerLogin(PlayerLoginEvent event){
+        Player player = event.getPlayer();
+        UUID uuid = player.getUniqueId();
+
+        UserMask u = User.getMask(this, uuid);
+        u.setVarTemp("soulbindOverride", false);
+    }
+
+
+    @OCmd(cmd = "soulbind %i", info = "Makes item soulbound to you")
     void commandBind(Player player, String args[]) {
         UUID uuid = player.getUniqueId();
         UserMask u = User.getMask(this, uuid);
@@ -72,7 +88,7 @@ public class Soulbind extends JavaModule implements ObeliskListener{
         Integer token = u.getVarElseSetDefault("token", 0);
 
         if(token > 0){
-            if(player.getInventory().getItemInMainHand() instanceof ItemStack){
+            if(player.getInventory().getItemInMainHand() != null){
                 if (player.getInventory().getItemInMainHand().getAmount() > 1) {
                     player.sendMessage("Maximum stack size for soulbound items is one.");
                 }
@@ -81,9 +97,10 @@ public class Soulbind extends JavaModule implements ObeliskListener{
                 }
                 else {
                     ItemStack item = player.getInventory().getItemInMainHand();
-                    ItemMeta itemmeta = item.getItemMeta();
-                    ArrayList<String> lore = new ArrayList<String>();
-                    item.setItemMeta(itemmeta);
+
+                    CustomTag tag = CustomTag.getFrom(item);
+                    tag.put("token", player.getUniqueId().toString());
+
                     u.setVar("token", token - lvl);
 
                     player.getInventory().setItemInMainHand(SoulbindEnchant.addBound(item, lvl));
@@ -97,42 +114,34 @@ public class Soulbind extends JavaModule implements ObeliskListener{
 
     @OCmd(cmd = "unsoulbind", info = "Makes item soulbound to you")
     void commandUnbind(Player player) {
-        UUID uuid = player.getUniqueId();
-        UserMask u = User.getMask(this, uuid);
 
-        Integer token = u.getVarElseSetDefault("token", 0);
-
-        if(player.getInventory().getItemInMainHand() instanceof ItemStack){
+        if(player.getInventory().getItemInMainHand() != null){
             if (player.getInventory().getItemInMainHand().getAmount() > 1) {
                 player.sendMessage("Maximum stack size for soulbound items is one.");
             }
             else if(player.getInventory().getItemInMainHand().containsEnchantment(Enchantment.getByName("Soulbound"))) {
                 ItemStack item = player.getInventory().getItemInMainHand();
-                ItemMeta itemmeta = item.getItemMeta();
-                ArrayList<String> lore = new ArrayList<String>();
-                item.setItemMeta(itemmeta);
-                Integer lvl = getEnchLevel(item);
-                if(lvl > 1) {
-                    u.setVar("token", token + lvl);
-                }
 
-                player.getInventory().setItemInMainHand(SoulbindEnchant.removeBound(item));
+                Integer lvl = getEnchLevel(item);
+
+                if(lvl > 1) {
+                    CustomTag tag = CustomTag.getFrom(item);
+                    String uuidToken = tag.get("token");
+
+                    UserMask u = User.getMask(this, UUID.fromString(uuidToken));
+                    Integer token = u.getVarElseSetDefault("token", 0);
+
+                    u.setVar("token", token + lvl);
+
+
+                    player.getInventory().setItemInMainHand(SoulbindEnchant.removeBound(item));
+                }
             }
             else {
                 player.sendMessage("This item is already unsoulbound");
             }
         }
     }
-
-
-    void setToken(Player player){
-        UUID uuid = player.getUniqueId();
-        UserMask u = User.getMask(this, uuid);
-
-        Integer token = u.getVarElseSetDefault("token", 0);
-        u.setVar("token", token + 1);
-    }
-
 
     @OCmd(cmd = "tokens", info = "View token")
     void getToken(Player player) {
@@ -144,215 +153,42 @@ public class Soulbind extends JavaModule implements ObeliskListener{
         player.sendMessage("You have this many soulbind token: " + Integer.toString(token));
     }
 
+    @OCmd(cmd = "override %P", info = "Override soulbound", perm = "soulbind.override")
+    void overrideSoulbind(Player player, String args[]) {
+        player.sendMessage("test");
 
-    @EventHandler
-    void onItemDrop(PlayerDropItemEvent event){
-        ItemStack item = event.getItemDrop().getItemStack();
+        for (Player pl : player.getWorld().getPlayers()) {
+            String name = ChatColor.stripColor(pl.getPlayerListName());
 
-        if(isSoulbound(item)){
-            event.setCancelled(true);
-            event.getPlayer().sendMessage(ChatColor.DARK_RED + "You cannot throw out soulbound items!");
-        }
-    }
+            if(name.equals(args[0])){
+                UUID uuid = pl.getUniqueId();
+                UserMask u = User.getMask(this, uuid);
 
-    @EventHandler
-    void onItemDrag(InventoryDragEvent event){
-        ItemStack item = event.getOldCursor();
+                Boolean test = u.getVarElseSetDefault("soulbindOverride", false);
 
-        if (isSoulbound(item)) {
-            event.setCancelled(true);
-            event.getWhoClicked().sendMessage(ChatColor.DARK_PURPLE + "You cannot move soulbound items to chests or storage.");
-        }
-    }
-
-    @EventHandler
-    public void onBlockPlace(BlockPlaceEvent event) {
-        ItemStack item = event.getItemInHand();
-
-        if(isSoulbound(item)){
-            event.setCancelled(true);
-            event.getPlayer().sendMessage(ChatColor.RED + "This item can not be placed, it is bound too tightly.");
-        }
-    }
-
-    @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event){
-        if(event.getItem() != null) {
-            ItemStack item = event.getItem();
-
-            if (isSoulbound(item)) {
-                event.setCancelled(true);
-                event.getPlayer().sendMessage(ChatColor.RED + "This item can not be used, it is bound too tightly.");
-            }
-        }
-    }
-
-    @EventHandler
-    void onPlayerDamageEntity(EntityDamageByEntityEvent event) {
-        if (event.getDamager() instanceof Player) {
-            Player player = (Player) event.getDamager();
-            if (isSoulbound(player.getInventory().getItemInMainHand())) {
-                event.setCancelled(true);
-                event.getDamager().sendMessage(ChatColor.RED + "This item can not be used, it is bound too tightly.");
-            }
-        }
-    }
-
-    @EventHandler
-    void onBowShoot(final EntityShootBowEvent event) {
-        if (event.getEntity() instanceof Player) {
-            Player player = (Player) event.getEntity();
-            Integer first = player.getInventory().first(Material.ARROW);
-            ItemStack item = player.getInventory().getItem(first);
-            if (isSoulbound(item)){
-                event.setCancelled(true);
-                event.getEntity().sendMessage(ChatColor.RED + "Why did you even soulbind an arrow? You can't use it");
-                player.updateInventory();
-            }
-        }
-    }
-
-
-    @EventHandler
-    public void onInventoryClick(InventoryClickEvent event){
-        ItemStack item = event.getCurrentItem();
-        Player player = (Player) event.getWhoClicked();
-        Inventory top = player.getOpenInventory().getTopInventory();
-
-        if(item == null){
-            return;
-        }
-
-        if((isArmor(item.getType()) && (event.getClick() == ClickType.SHIFT_RIGHT || event.getClick() == ClickType.SHIFT_LEFT)) || ((event.getClick() == ClickType.SHIFT_RIGHT || event.getClick() == ClickType.SHIFT_LEFT) && top.getType() != InventoryType.CRAFTING)) {
-            if (isSoulbound(item)) {
-                event.setCancelled(true);
-                player.sendMessage(ChatColor.DARK_PURPLE + "You cannot move soulbound items to chests or storage or equip as armor.");
-            }
-        }
-
-        if(event.getClickedInventory() != player.getInventory()){
-            item = event.getCursor();
-            if (isSoulbound(item)) {
-                event.setCancelled(true);
-                player.sendMessage(ChatColor.DARK_PURPLE + "You cannot move soulbound items to chests or storage.");
-            }
-        }
-
-        if(event.getSlotType() == InventoryType.SlotType.ARMOR){
-            item = event.getCursor();
-            if (isSoulbound(item)) {
-                event.setCancelled(true);
-                player.sendMessage(ChatColor.DARK_PURPLE + "You cannot move soulbound items to chests or storage.");
-            }
-        }
-    }
-
-    @EventHandler
-    void onPlayerDeath(PlayerDeathEvent event){
-        Boolean playerKilled = false;
-
-
-        if(event.getEntity().getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK){
-            if(event.getEntity() instanceof Player){
-                playerKilled = true;
-            }
-        }
-
-        ItemStack [] inventory;
-        List<ItemStack> inventoryKeep = new ArrayList<ItemStack>();
-
-        Player player = event.getEntity();
-        UserMask u = User.getMask(this, player.getUniqueId());
-        Boolean remove = true;
-
-        player.sendMessage(event.getDrops().toString());
-
-        while (remove) {
-            remove = false;
-            for(ItemStack item : event.getDrops()){
-            ItemMeta meta = item.getItemMeta();
-            if(isSoulbound(item)) {
-                Integer lvl = getEnchLevel(item);
-
-                switch(lvl) {
-                    case 1:
-                        if(playerKilled == false) {
-                            inventoryKeep.add(item);
-                            event.getDrops().remove(item);
-                        }
-                        item.removeEnchantment(ench);
-                        break;
-                    case 2:
-                        item.removeEnchantment(ench);
-                        inventoryKeep.add(item);
-                        event.getDrops().remove(item);
-                        break;
-                    case 3:
-                        inventoryKeep.add(item);
-                        event.getDrops().remove(item);
-                        break;
-                    case 4:
-                        inventoryKeep.add(item);
-                        event.getDrops().remove(item);
-                        break;
+                if(!test){
+                    u.setVarTemp("soulbindOverride", true);
+                    player.sendMessage("Soulbind override for " + name + " has been toggled on");
                 }
-
-                remove = true;
-                break;
+                else {
+                    u.setVarTemp("soulbindOverride", false);
+                    player.sendMessage("Soulbind override for " + name + " has been toggled off");
                 }
             }
         }
-
-        if (inventoryKeep.size() > 0) {
-            u.setVar("items", inventoryKeep);
-            player.sendMessage(inventoryKeep.toString());
-        }
-    }
-
-    @EventHandler
-    public void onPlayerSpawn(PlayerRespawnEvent event) {
-        Player player = event.getPlayer();
-        UserMask u = User.getMask(this, player.getUniqueId());
-
-        ArrayList<ItemStack> items = u.getVarElseSetDefault("items", null);
-        Optional.ofNullable(items).ifPresent(value -> {for(ItemStack item: value){player.getInventory().addItem(item);}});
-    }
-
-    public Integer getEnchLevel(ItemStack item){
-        Integer lvl = item.getEnchantmentLevel(ench);
-        return lvl;
     }
 
 
+    private void setToken(Player player, Integer num){
+        UUID uuid = player.getUniqueId();
+        UserMask u = User.getMask(this, uuid);
 
-    private boolean isArmor(Material mat) {
-        switch (mat) {
-            case JACK_O_LANTERN:
-            case GOLD_HELMET:
-            case GOLD_BOOTS:
-            case GOLD_CHESTPLATE:
-            case GOLD_LEGGINGS:
-            case LEATHER_HELMET:
-            case LEATHER_CHESTPLATE:
-            case LEATHER_BOOTS:
-            case LEATHER_LEGGINGS:
-            case CHAINMAIL_HELMET:
-            case CHAINMAIL_BOOTS:
-            case CHAINMAIL_CHESTPLATE:
-            case CHAINMAIL_LEGGINGS:
-            case IRON_HELMET:
-            case IRON_BOOTS:
-            case IRON_CHESTPLATE:
-            case IRON_LEGGINGS:
-            case DIAMOND_HELMET:
-            case DIAMOND_BOOTS:
-            case DIAMOND_CHESTPLATE:
-            case DIAMOND_LEGGINGS:
-            case ELYTRA:
-                return true;
-            default:
-                return false;
-        }
+        Integer token = u.getVarElseSetDefault("token", 0);
+        u.setVar("token", token + num);
+    }
+
+    private Integer getEnchLevel(ItemStack item){
+        return item.getEnchantmentLevel(ench);
     }
 }
 
---
+++ src/main/java/io/github/haintrain/soulbind/SoulbindListeners.java
@@ -0,0 +1,275 @@
+package io.github.haintrain.soulbind;
+
+import net.minegrid.obelisk.api.Module;
+import net.minegrid.obelisk.api.User;
+import net.minegrid.obelisk.api.UserMask;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.inventory.*;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import static io.github.haintrain.soulbind.SoulbindEnchant.isSoulbound;
+
+public class SoulbindListeners implements Listener{
+
+    private SoulbindEnchant ench;
+    private Module mod;
+
+    public SoulbindListeners(SoulbindEnchant ench, Module mod){
+        this.ench = ench;
+        this.mod = mod;
+    }
+
+    @EventHandler
+    void onItemDrop(PlayerDropItemEvent event){
+        ItemStack item = event.getItemDrop().getItemStack();
+        Player player = event.getPlayer();
+
+        UUID uuid = player.getUniqueId();
+        UserMask u = User.getMask(mod, uuid);
+        Boolean override = u.getVarElseSetDefault("soulbindOverride", false);
+        player.sendMessage(override.toString());
+
+        if(isSoulbound(item) && !override){
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.DARK_RED + "You cannot throw out soulbound items!");
+        }
+    }
+
+    @EventHandler
+    void onItemDrag(InventoryDragEvent event){
+        ItemStack item = event.getOldCursor();
+        Player player = (Player) event.getWhoClicked();
+
+        UUID uuid = player.getUniqueId();
+        UserMask u = User.getMask(mod, uuid);
+        Boolean override = u.getVarElseSetDefault("soulbindOverride", false);
+
+
+        if (isSoulbound(item) && !override) {
+            event.setCancelled(true);
+            event.getWhoClicked().sendMessage(ChatColor.DARK_PURPLE + "You cannot move soulbound items to chests or storage.");
+        }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        ItemStack item = event.getItemInHand();
+        Player player = event.getPlayer();
+
+        UUID uuid = player.getUniqueId();
+        UserMask u = User.getMask(mod, uuid);
+        Boolean override = u.getVarElseSetDefault("soulbindOverride", false);
+
+        if(isSoulbound(item) && !override){
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.RED + "This item can not be placed, it is bound too tightly.");
+        }
+    }
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event){
+        if(event.getItem() != null) {
+            ItemStack item = event.getItem();
+
+            if (isSoulbound(item)) {
+                event.setCancelled(true);
+                event.getPlayer().sendMessage(ChatColor.RED + "This item can not be used, it is bound too tightly.");
+            }
+        }
+    }
+
+    @EventHandler
+    void onPlayerDamageEntity(EntityDamageByEntityEvent event) {
+        if (event.getDamager() instanceof Player) {
+            Player player = (Player) event.getDamager();
+            if (isSoulbound(player.getInventory().getItemInMainHand())) {
+                event.setCancelled(true);
+                event.getDamager().sendMessage(ChatColor.RED + "This item can not be used, it is bound too tightly.");
+            }
+        }
+    }
+
+    @EventHandler
+    void onBowShoot(final EntityShootBowEvent event) {
+        if (event.getEntity() instanceof Player) {
+            Player player = (Player) event.getEntity();
+            Integer first = player.getInventory().first(Material.ARROW);
+            ItemStack item = player.getInventory().getItem(first);
+            if (isSoulbound(item)){
+                event.setCancelled(true);
+                event.getEntity().sendMessage(ChatColor.RED + "Why did you even soulbind an arrow? You can't use it");
+                player.updateInventory();
+            }
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event){
+        Player player = (Player) event.getWhoClicked();
+
+        UUID uuid = player.getUniqueId();
+        UserMask u = User.getMask(mod, uuid);
+
+        ItemStack item = event.getCurrentItem();
+        Inventory top = player.getOpenInventory().getTopInventory();
+        Boolean override = u.getVarElseSetDefault("soulbindOverride", false);
+
+        if(!override) {
+            if (item == null) {
+                return;
+            }
+
+            if ((isArmor(item.getType()) && (event.getClick() == ClickType.SHIFT_RIGHT || event.getClick() == ClickType.SHIFT_LEFT)) || ((event.getClick() == ClickType.SHIFT_RIGHT || event.getClick() == ClickType.SHIFT_LEFT) && top.getType() != InventoryType.CRAFTING)) {
+                if (isSoulbound(item)) {
+                    event.setCancelled(true);
+                    player.sendMessage(ChatColor.DARK_PURPLE + "You cannot move soulbound items to chests or storage or equip as armor.");
+                }
+            }
+            else if (event.getSlotType() == InventoryType.SlotType.ARMOR) {
+                item = event.getCursor();
+                if (isSoulbound(item)) {
+                    event.setCancelled(true);
+                    player.sendMessage(ChatColor.DARK_PURPLE + "You cannot move soulbound items to chests or storage.");
+                }
+            }
+            else if (event.getAction() == InventoryAction.HOTBAR_SWAP || event.getAction() == InventoryAction.HOTBAR_MOVE_AND_READD)  {
+                int num = event.getHotbarButton();
+                item = player.getInventory().getItem(num);
+                if (isSoulbound(item)) {
+                    event.setCancelled(true);
+                    player.sendMessage(ChatColor.DARK_PURPLE + "You cannot move soulbound items to chests or storage.");
+                }
+            }
+            else if (event.getClickedInventory() != player.getInventory()) {
+                item = event.getCursor();
+                if (isSoulbound(item)) {
+                    event.setCancelled(true);
+                    player.sendMessage(ChatColor.DARK_PURPLE + "You cannot move soulbound items to chests or storage.");
+                }
+            }
+        }
+    }
+
+
+    @EventHandler
+    void onPlayerDeath(PlayerDeathEvent event){
+        Boolean playerKilled = false;
+
+
+        if(event.getEntity().getLastDamageCause().getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK){
+            if(event.getEntity() instanceof Player){
+                playerKilled = true;
+            }
+        }
+
+        List<ItemStack> inventoryKeep = new ArrayList<>();
+
+        Player player = event.getEntity();
+        UserMask u = User.getMask(mod, player.getUniqueId());
+        Boolean remove = true;
+
+        player.sendMessage(event.getDrops().toString());
+
+        while (remove) {
+            remove = false;
+            for(ItemStack item : event.getDrops()){
+                if(isSoulbound(item)) {
+                    Integer lvl = getEnchLevel(item);
+
+                    switch(lvl) {
+                        case 1:
+                            if(!playerKilled) {
+                                inventoryKeep.add(item);
+                                event.getDrops().remove(item);
+                            }
+                            item.removeEnchantment(ench);
+                            break;
+                        case 2:
+                            item.removeEnchantment(ench);
+                            inventoryKeep.add(item);
+                            event.getDrops().remove(item);
+                            break;
+                        case 3:
+                            inventoryKeep.add(item);
+                            event.getDrops().remove(item);
+                            break;
+                        case 4:
+                            inventoryKeep.add(item);
+                            event.getDrops().remove(item);
+                            break;
+                    }
+
+                    remove = true;
+                    break;
+                }
+            }
+        }
+
+        if (inventoryKeep.size() > 0) {
+            u.setVar("items", inventoryKeep);
+            player.sendMessage(inventoryKeep.toString());
+        }
+    }
+
+    @EventHandler
+    public void onPlayerSpawn(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+        UserMask u = User.getMask(mod, player.getUniqueId());
+
+        ArrayList<ItemStack> items = u.getVarElseSetDefault("items", null);
+        Optional.ofNullable(items).ifPresent(value -> {for(ItemStack item: value){player.getInventory().addItem(item);}});
+    }
+
+    private Integer getEnchLevel(ItemStack item){
+        return item.getEnchantmentLevel(ench);
+    }
+
+
+    private boolean isArmor(Material mat) {
+        switch (mat) {
+            case JACK_O_LANTERN:
+            case GOLD_HELMET:
+            case GOLD_BOOTS:
+            case GOLD_CHESTPLATE:
+            case GOLD_LEGGINGS:
+            case LEATHER_HELMET:
+            case LEATHER_CHESTPLATE:
+            case LEATHER_BOOTS:
+            case LEATHER_LEGGINGS:
+            case CHAINMAIL_HELMET:
+            case CHAINMAIL_BOOTS:
+            case CHAINMAIL_CHESTPLATE:
+            case CHAINMAIL_LEGGINGS:
+            case IRON_HELMET:
+            case IRON_BOOTS:
+            case IRON_CHESTPLATE:
+            case IRON_LEGGINGS:
+            case DIAMOND_HELMET:
+            case DIAMOND_BOOTS:
+            case DIAMOND_CHESTPLATE:
+            case DIAMOND_LEGGINGS:
+            case ELYTRA:
+                return true;
+            default:
+                return false;
+        }
+    }
+}
